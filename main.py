87e40711e101e40164c05d7339c6e00911a95466
Max <107602115+max-glass@users.noreply.github.com>
2022-07-30 20:44:33 -0400

    Fixed spacing and minor typos.

--- main.py
+++ main.py
@@ -9,26 +9,46 @@
 # mxGlass
 # Added shuffle button.
 
+# 30JUL2022
+# BB31420
+# Added total card count and nuetral card logging.
+
 import tkinter as tk
 from tkinter import ttk
 
 # initialize variables
 count = 0
+cardsPlayed = 0
 log = []
 
 def clicked(): # without event because I use `command=` instead of `bind`
     global count
+    global cardsPlayed
 
     count = count + 1
+    cardsPlayed = cardsPlayed + 1
 
     label1.configure(text=f'Count = {count}')
+    label2.configure(text=f'Cards Played = {cardsPlayed}')
 
 def clicked_neg():
     global count
+    global cardsPlayed
 
     count = count - 1
+    cardsPlayed = cardsPlayed + 1
 
     label1.configure(text=f'Count = {count}')
+    label2.configure(text=f'Cards Played = {cardsPlayed}')
+
+def clicked_null():
+    global count
+    global cardsPlayed
+
+    cardsPlayed = cardsPlayed + 1
+
+    label1.configure(text=f'Count = {count}')
+    label2.configure(text=f'Cards Played = {cardsPlayed}')     
 
 def add_log(x):
     global log
@@ -36,15 +56,19 @@ def add_log(x):
 
 def shuffle():
     global count
+    global cardsPlayed
+    
+    cardsPlayed = 0
     count = 0
-    label1.configure(text=f'Count = {count}')
 
+    label1.configure(text=f'Count = {count}')
+    label2.configure(text=f'Cards Played = {cardsPlayed}')
 
 windows = tk.Tk()
 windows.title("")
 
 label = tk.Label(windows, text="Blackjack card counter")
-label.grid(column=0, row=0)
+label.grid(column=1, row=0)
 
 logLabel = tk.Label(windows, text="Card log:")
 logLabel.grid(column=2, row=10)
@@ -56,7 +80,10 @@ label = tk.Label(windows, text="")
 label.grid(column=4, row=0)
 
 label1 = tk.Label(windows)
-label1.grid(column=0, row=1)
+label1.grid(column=2, row=4)
+
+label2 = tk.Label(windows)
+label2.grid(column=2, row=9)
 
 # Plus cards
 custom_button = ttk.Button(windows, text="2", command=lambda: [clicked(), add_log("2")])
@@ -73,6 +100,16 @@ custom_button.grid(column=0, row=5)
 
 custom_button = ttk.Button(windows, text="6", command=lambda: [clicked(), add_log("6")])
 custom_button.grid(column=0, row=6)
+
+# Neutral cards
+custom_button = ttk.Button(windows, text="7", command=lambda: [clicked_null(), add_log("7")])
+custom_button.grid(column=0, row=7)
+
+custom_button = ttk.Button(windows, text="8", command=lambda: [clicked_null(), add_log("8")])
+custom_button.grid(column=0, row=8)
+
+custom_button = ttk.Button(windows, text="9", command=lambda: [clicked_null(), add_log("9")])
+custom_button.grid(column=0, row=9)
 
 # Minus cards
 custom_button = ttk.Button(windows, text="10", command=lambda: [clicked_neg(), add_log("10")])
@@ -92,7 +129,6 @@ custom_button.grid(column=1, row=6)
 
 # Shuffle button
 custom_button = ttk.Button(windows, text="Shuffle", command=lambda: [shuffle()])
-custom_button.grid(column=2, row=7)
+custom_button.grid(column=2, row=2)
 
 windows.mainloop()
-
