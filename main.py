d1bbde10f59806a29e65b5abeae86ec83d3aa38a
Max <107602115+max-glass@users.noreply.github.com>
2022-08-01 18:54:53 -0400

    Added TODO.

--- main.py
+++ main.py
@@ -22,10 +22,12 @@
 # Added variable number of decks
 
 
-# TODO: Implement win/loss script into GUI (mxGlass). 
+# TODO: Implement win/loss script into GUI (mxGlass).
+# TODO: Make font bigger on GUI.
 # TODO: Determine formula/function to predict good bet amount (taking account of record, balance, count, etc.).
 # TODO: Clear win/loss fields after clicking "calculate."
 # TODO: Maybe we link this to hardware? ... Think about it.
+
 
 # Import necessary libraries
 # External dependencies
