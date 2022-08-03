c03238902da2bd15ae03be059b78736c28805bc1
Max <107602115+max-glass@users.noreply.github.com>
2022-08-02 23:18:34 -0400

    Fixed spacing, removed Blackjack_score_keeper.py.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+
 # Blackjack card counter
 mxGlass
 
--- strategy.py
+++ strategy.py
@@ -1,8 +1,9 @@
-def basic_strategy(player_total, dealer_value, soft):
-    """ This is a simple implementation of Blackjack's
-        basic strategy. It is used to recommend actions
-        for the player. """
 
+# This is a simple implementation of Blackjack's
+# basic strategy. It is used to recommend actions
+# for the player.
+
+def basic_strategy(player_total, dealer_value, soft):
     if 4 <= player_total <= 8:
         return 'hit'
     if player_total == 9:
