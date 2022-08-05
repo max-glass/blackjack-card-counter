b23bdfd750fafea1b58d546b10955a087de49541
Max <107602115+max-glass@users.noreply.github.com>
2022-08-04 22:34:30 -0400

    Implemented logging (logger.txt)

--- logger.py
+++ logger.py
@@ -6,7 +6,13 @@
 # 04AUG22
 # Initial commit.
 
+# mxGlass
+# 04AUG22
+# Implemented logging (all played cards from any game can be found in logger.txt).
+
 def logger(card):
-    with open('logger.txt', 'w') as f:
-        f.write(card)
+    with open('logger.txt', 'a') as f:
+        f.write(card + "\n")
+        # f.write("\n")
+    f.close()
 
